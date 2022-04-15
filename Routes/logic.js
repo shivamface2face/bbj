b0849df0abfe3f602a38fadff436b4f14cc591f2
Shivam Singh <shivamfaceuit@gmail.com>
2022-04-15 14:31:29 +0530

    done

---
+++ Routes/logic.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post("/bfhl", (req, res, next) => {
+    const {data} = req.body;
+    if(!data || Array.isArray(data)){
+        return res.status(400)
+        .send({
+            "is_Success": false,
+            "user_id": "Deepansu_Gupta_13061998",
+            "msg": "An array is required"
+        })
+    }
+
+    let alphabets = [];
+    let numbers = [];
+    data.map((ele)=> {
+        if(isNaN(ele)){
+            alphabets.push(ele);
+        } else{numbers.push(ele);}
+    })
+
+    res.send({
+        "is_success": true,
+        "user_id": "Deepansu_Gupta_13061998",
+        "email": "dkgupta8691@gmail.com",
+        "roll_number": "0101CS191041",
+        "numbers": numArray,
+        "alphabets": alphaArray
+    })
+})
+
+module.exports = router;
